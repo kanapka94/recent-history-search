0eff58320ecd96855e9f8c7ab1df024d862fc488
Adam <rumpelteam@gmail.com>
2024-02-15 12:18:46 +0100

    improve options page

--- src/options/options.ts
+++ src/options/options.ts
@@ -1,19 +1,37 @@
+import { subYears } from 'date-fns';
+
 document.addEventListener('DOMContentLoaded', function () {
   const elForm = document.getElementById('settings-form');
+  const searchDateInput = document.getElementById('search-date');
+
+  setDateFromSettings();
+  setInitialDate();
 
   elForm.addEventListener('submit', function (event) {
     event.preventDefault();
 
-    const searchDate = document.getElementById('search-date').value;
+    const searchDate = searchDateInput.value;
 
     chrome.storage.local.set({ searchLimitDate: searchDate }).then(() => {
       console.log('Zapisano datę: ' + searchDate);
     });
   });
 
-  chrome.storage.local.get(['searchLimitDate'], function (result) {
-    if (result) {
-      document.getElementById('search-date').value = result.searchLimitDate;
+  function setDateFromSettings() {
+    chrome.storage.local.get(['searchLimitDate'], function (result) {
+      if (result) {
+        searchDateInput.value = result.searchLimitDate;
+      }
+    });
+  }
+
+  function setInitialDate() {
+    if (!searchDateInput.value) {
+      searchDateInput.value = subYears(new Date(), 1).toISOString().split('T')[0];
     }
-  });
+
+    chrome.storage.local.set({ searchLimitDate: searchDateInput.value }).then(() => {
+      console.log('Zapisano datę: ' + searchDateInput.value);
+    });
+  }
 });
